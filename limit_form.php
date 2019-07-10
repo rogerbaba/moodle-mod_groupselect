68ddcc3f535b58220e9fb42aad30b5a99b4b2772
Sagar Ghimire <sagarghimire@catalyst-au.net>
2019-07-10 11:05:38 +1000

    issue#97: add limits per group
    Add a new table
    * Store groupselectid and groupid
    * Store limit per group
    
    Add view tabs
    * Tabs.php for the tabs
    * Update land strings for tabs
    
    Add Form and limit page
    
    Update Form
    * Use groupid as key and limit as value
    
    Update form and handler
    * Fix according to Moodle coding standard
    
    Add to limits table
    * Add SQL to insert limit to table
    * Update records if exists
    
    Update view logic
    * Add conditions to check for adding infinite members
    
    Add override group limit permission
    
    Add backup and restore functionality.

---
+++ limit_form.php
@@ -0,0 +1,100 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * A form for setting group limits
+ *
+ * @package   mod_groupselect
+ * @author    Sagar Ghimire <sagarghimire@catalyst-au.net>
+ * @copyright Catalyst iT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
+
+defined('MOODLE_INTERNAL') || die;
+
+require_once($CFG->dirroot.'/lib/formslib.php');
+
+/**
+ * GroupSelect Limit Form Class
+ *
+ * @package   mod_groupselect
+ * @author    Sagar Ghimire <sagarghimire@catalyst-au.net>
+ * @copyright Catalyst IT
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
+ */
+class limit_form extends moodleform {
+
+    /**
+     * Definition of the form
+     */
+    public function definition() {
+        global $COURSE;
+
+        $rows = groups_get_all_groups($COURSE->id, 0, $this->_customdata['targetgrouping']);
+        $keys = array_keys($rows);
+        $mform =& $this->_form;
+        $count = count($rows);
+
+        for ($i = 0; $i < $count; $i++) {
+            $limitnum = 'limit_'.$rows[$keys[$i]]->id;
+
+            $mform->addElement('text', $limitnum, $rows[$keys[$i]]->name, 'maxlength="50" size="5"');
+            $mform->addHelpButton($limitnum, 'limits', 'mod_groupselect');
+            $mform->setType($limitnum, PARAM_TEXT);
+            $mform->addRule($limitnum, get_string('validnum', 'mod_groupselect'), 'numeric', '');
+
+        }
+
+        // Hidden Params.
+        $mform->addElement('hidden', 'instanceid');
+        $mform->setType('instanceid', PARAM_INT);
+        $mform->setDefault('instanceid', $this->_customdata['instanceid']);
+
+        $mform->addElement('hidden', 'num');
+        $mform->setDefault('num', $count);
+        $mform->setType('num', PARAM_INT);
+
+        // Buttons.
+        $this->add_action_buttons();
+    }
+
+    /**
+     * Form validation
+     *
+     * @param array $data
+     * @param array $files
+     * @return array $errors An array of validataion errors for the form.
+     */
+    public function validation($data, $files) {
+        global $DB;
+        $errors = parent::validation($data, $files);
+        $keys = array_keys($data);
+        $num = $data['num'];
+
+        $minmembers = $DB->get_field('groupselect', 'minmembers', array('id' => $data['instanceid']));
+
+        for ($i = 0; $i < $num; $i++) {
+            if ($data[$keys[$i]] < $minmembers && $data[$keys[$i]] != 0) {
+                $errors[$keys[$i]] = get_string('validmax', 'mod_groupselect', $minmembers);
+            }
+        }
+
+        return $errors;
+    }
+}
